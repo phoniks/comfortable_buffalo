1373d322809eb0e1495949eb0481d909e9fdb971
Devon Wesley <dwesley89@gmail.com>
2016-08-29 17:24:43 -0700

    added homepage route

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,7 @@ const router = express.Router()
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  Book.all()
-    .then((books) => {
-
-      console.log('All Books', books);
-      res.render('homepage', { books })
-    })
+  Book.all().then( (books) => res.render('homepage', { books }) )
 })
 
 module.exports = router
