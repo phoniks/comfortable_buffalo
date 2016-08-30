3db535c5c5c94696b41a6c06b81e64729a002188
Devon Wesley <dwesley89@gmail.com>
2016-08-29 17:19:39 -0700

    added books to homepage

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
+import database, { Book } from '../database'
 import express from 'express'
 const router = express.Router()
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' })
+  Book.all()
+    .then((books) => {
+
+      console.log('All Books', books);
+      res.render('homepage', { books })
+    })
 })
 
 module.exports = router
