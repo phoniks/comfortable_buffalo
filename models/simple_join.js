632e75079ea08976efc25088730f092d688a3990
Devon Wesley <dwesley89@gmail.com>
2016-08-31 13:25:17 -0700

    jrob helped with SimpleJoin

--- models/simple_join.js
+++ models/simple_join.js
@@ -1,38 +1,42 @@
 class SimpleJoin {
-  constructor (table,options={}){
-    if( table === undefined  ) {
-      throw 'Table parameter is required'
-    } else if( typeof( table ) !== 'string' ) {
-      throw( 'Table parameter must be string' )
-    }
-
+  constructor( table, columns, joins, filter ){
     this.table = table
-    this.fields = options.fields || []
-    this.filter = options.where || []
-
-    this.combine = options.join[0] || []
-    this.on = options.on || []
-    this.id = options.in || []
+    this.columns = columns
+    this.joins = joins
+    this.filter = filter
   }
 
   toString() {
-    return `SELECT ${this.sqlFields()} FROM ${this.table} JOIN ${this.combine} ${this.onSql()} ${this.where()}`
+    // SELECT books.*, authors.name, authors.id as author_id
+    // JOIN book_authors ON book_authors.book_id=books.id JOIN authors ON book_authors.author_id=authors.id
+    // WHERE book_authors.author_id=1"
+    return `SELECT ${this.sqlColumns()} FROM ${this.table} ${this.sqlJoins()} ${this.where()}`
   }
 
-  // SELECT books.* FROM books JOIN author_books ON books.id = author_books.book_id WHERE author_books.author_id=$1
-
-  sqlFields() {
-    return this.fields.map(
-      field => field.indexOf( '.' ) === -1 ? `${field}.*` : field
-    ).join( ', ' )
+  sqlColumns() {
+    return Object.keys( this.columns ).map( table => {
+      return this.mergeFields( table, this.columns[ table ] )
+    }).join( ', ' )
   }
 
-  onSql(){
-    return `ON ${this.on[1]} = ${this.on[0]}`
+  mergeFields( table, fields ) {
+    return fields.map( entry => {
+      if( typeof( entry ) === 'string' ) {
+        return `${table}.${entry}`
+      } else {
+        return `${table}.${entry[ 0 ]} as ${entry[ 1 ]}`
+      }
+    }).join( ', ' )
+  }
+
+  sqlJoins() {
+    return Object.keys( this.joins ).map( joinTable => {
+      return `JOIN ${joinTable} ON ${this.joins[ joinTable ][0]}=${this.joins[ joinTable ][1]}`
+    }).join( ' ' )
   }
 
   where() {
-    return `WHERE ${this.filter[0].field_id}`
+    return `WHERE ${this.filter.join( '=' )}`
   }
 }
 
