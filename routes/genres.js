480cf2e70ffdbc0aa1b2cdfb30171f2339b8e1e3
Devon Wesley <dwesley89@gmail.com>
2016-09-01 15:11:06 -0700

    able to edit genre desciptions

--- routes/genres.js
+++ routes/genres.js
@@ -7,8 +7,22 @@ router.get('/:id', ( req, res ) => {
   Promise.all([ Genre.findOne(id), Genre.getBooks(id) ])
     .then(results => {
       const [ genre, books ] = results
-      res.render('genre_details', { genre, books })
+      res.render('genre_details', { genre, books, genre_id: id })
     })
+})
+
+router.get('/edit/:id', ( req, res ) => {
+  const { id } = req.params
+  Genre.findOne(id).then(genre => {
+    res.render('edit_genre', { genre, id })
+  })
+})
+
+router.post('/edit_genre/:id', ( req, res ) => {
+  const { id } = req.params
+  Genre.update(parseInt(id), req.body).then( genre_id => {
+    res.redirect( `/genres/${id}` )
+  })
 })
 
 export default router
