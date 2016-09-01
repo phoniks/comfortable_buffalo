b1bcf21faf2e0c1802063bf75f439b5013507818
John Hopkins <johnthopkins@gmail.com>
2016-09-01 12:30:07 -0700

    merging

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ router.get('/', (req, res, next) => {
   Promise.all([ Book.all(), Author.all(), Genre.all()])
     .then( results => {
       const [ books, authors, genres ] = results
+      console.log(books)
       res.render( 'homepage', { books, authors, genres })
     })
 })
